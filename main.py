2de0ade03e0739514b8ffa38b2d5971f62a23af8
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-25 20:35:41 +0530

    saving jobs to files

--- main.py
+++ main.py
@@ -10,17 +10,19 @@ def find_jobs():
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
     soup = BeautifulSoup(html_text, 'lxml')
     jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-    for job in jobs:
+    for index, job in enumerate(jobs):
         published_date = job.find('span', class_='sim-posted').span.text
         if 'few' in published_date:
             company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
             skills = job.find('span', class_='srp-skills').text.replace(' ', '')
             more_info = job.header.h2.a['href']
             if unfamiliar_skill not in skills:
-                print(f"Company Name: {company_name}")
-                print(f"Required Skills: {skills}")
-                print(f'More info: {more_info}')
-                print('')
+                with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f"Company Name: {company_name.strip()} \n")
+                    f.write(f"Required Skills: {skills.strip()} \n")
+                    f.write(f'More info: {more_info} \n')
+                print(f'File Saved: {index}.txt')
+                
 
 
 
